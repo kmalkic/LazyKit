5b636ce601971fe1ce845a6e8e3ece6137c4051c
Kevin Malkic <k_malkic@yahoo.fr>
2016-01-18 13:55:54 +0000

    readme file

--- LazyKitDemo/LazyKitDemo/AppDelegate.swift
+++ LazyKitDemo/LazyKitDemo/AppDelegate.swift
@@ -28,7 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window!.rootViewController = MyViewControllerCssTest()
         window!.makeKeyAndVisible()
         
-        
         return true
     }
 
